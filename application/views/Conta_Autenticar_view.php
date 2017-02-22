6bb1f8f34ec70f94085166f5b872c0fb2139f188
Eduardo Luiz Natividade <elnatividade@gmail.com>
2017-02-22 08:13:33 -0300

    Commit 02
    Interface aplicada

--- application/views/Conta_Autenticar_view.php
+++ application/views/Conta_Autenticar_view.php
@@ -1,62 +1,105 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       
-    <title><?php echo $titulo ?></title>
-           
-	<link rel="stylesheet" type="text/css" media="screen" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	
 
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<html lang="pt-br">
+
+<head>
+
+	<meta content="text/html; charset=UTF-8" http-equiv="content-type">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	
+	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" media="screen"  />
+	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('application/views/assets/css/style.css') ?>" />
+		
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
+	<script src="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>
+	
+	<title><?php echo $titulo ?></title>
 	
 </head>
+
 <body>
+
 	<div class="container-fluid">
-		<div class="row-fluid">
-			<div class="col-md-12">
-				<div class="page-header">
-	  				<h1><?php echo $titulo ?></h1>
-				</div>
-			</div>
-		</div>	
-		<?php  if(isset($mensagem)): ?>
-		<div class="row-fluid">
-			<div class="col-md-4 col-md-offset-4">
-				<div class='<?php echo $classe_mensagem ?>' role='alert'><?php echo $mensagem ?></div>
-			</div>
-		</div>
-		<?php  endif; ?>
-		<form action="<?php echo base_url('Conta/autenticar') ?>" method="post" name="login" >
+	
+		<!-- header-->
 			<div class="row-fluid">
-				<div class="col-md-4 col-md-offset-4">
-					<div class="form-group">
-						<label for="nome_cliente">Cliente</label>
-						<input type="text" class="form-control" name="nome_cliente" value="" id="nome_cliente" placeholder="Nome do Cliente">
+				<div class="col-md-12">
+					<div class="masthead">
+						<div class="container-head">
+							<h1></h1>
+						</div>
 					</div>
-					<div class="form-group">
-						<label for="senha_cliente">Senha</label>
-						<input type="password" class="form-control" name="senha_cliente" value="" id="senha_cliente" placeholder="Senha do Cliente" >
-					</div>
-				</div>
-			</div>
+				</div><!-- /col-md-12 -->
+			</div><!-- /row-fluid -->
+		<!-- /header-->
+		
+		<!-- main content -->
 			<div class="row-fluid">
-				<div class="col-md-4 col-md-offset-4">
-					<button type="submit" class="btn btn-default btn-block" name="enviar" id="enviar" value="enviar">Acessar <span class="fa fa-sign-in" aria-hidden="true"></span></button>
-				</div>
-			</div>
-		</form>
-	</div>
+					<div class="col-md-12">
+					
+						<div class="panel panel panel-info">
+							<div class="panel-heading"><h4 class="text-center"><strong><?php echo $titulo ?></strong></h4></div>
+						<div class="panel-body">
+						
+							<div class="col-md-4 col-md-offset-4">
+								<div class="panel panel-default">
+	  								<div class="panel-body">
+	    								
+	    								
+	    								<form action="<?php echo base_url('Conta/autenticar') ?>" method="post" name="login" >
+								
+											<!-- nome cliente -->
+											<div class="form-group">
+												<label for="nome_cliente">Cliente</label>
+												<input type="text" class="form-control" name="nome_cliente" value="" id="nome_cliente" placeholder="Nome do Cliente">
+											</div>
+											<!-- /nome cliente -->
+											
+											<!-- senha cliente -->
+											<div class="form-group">
+												<label for="senha_cliente">Senha</label>
+												<input type="password" class="form-control" name="senha_cliente" value="" id="senha_cliente" placeholder="Senha do Cliente" >
+											</div>
+											<!-- /senha cliente -->
+											
+											<!-- botao enviar -->
+											<button type="submit" class="btn btn-primary btn-block" name="enviar" id="enviar" value="enviar">Acessar <span class="fa fa-sign-in" aria-hidden="true"></span></button>
+											<!-- /botao enviar -->
+																	
+										</form>
+	    							    								
+	  								</div>
+								</div> 
+							</div>
+						
+						
+							
+						</div><!-- /painel-body -->
+					</div><!-- /painel -->
+																
+				</div><!-- /col-md-4 col-md-offset-4 -->
+			</div><!-- row-fluid -->
+		<!-- /main content -->
+		
+	</div><!-- /container -->
+
 </body>
+
 </html>
--- application/views/Pesquisa_Exibir_view.php
+++ application/views/Pesquisa_Exibir_view.php
@@ -1,70 +1,82 @@
 <!DOCTYPE html>
+
 <html lang="pt-br">
+
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       
-    <title><?php echo $titulo ?></title>
-           
+
+<meta content="text/html; charset=UTF-8" http-equiv="content-type">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	
 	<link rel="stylesheet" type="text/css" media="screen" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<link href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('application/views/assets/css/style.css') ?>" />
+		
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
+	<script src="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>
+	
+	<title><?php echo $titulo ?></title>
 	
 </head>
+
 <body>
-	<!-- CONTAINER -->
+
 	<div class="container-fluid">
-	
-		<!-- HEADER -->
-		<div class="row">
-			<div class="col-md-12">
-				<div class="page-header">
-					<h1><?php echo $titulo ?><br /><small><?php echo $nome_cliente ?></small><a class="btn btn-default btn-sm pull-right" href="<?php echo base_url('Conta/desconectar')?>" role="button">Sair  <span class="fa fa-sign-out" aria-hidden="true"></span></a></h1>
-				</div>
-			</div>
-		</div>
-		<!-- HEADER -->
+		<!-- header-->
+			<div class="row-fluid">
+				<div class="col-md-12">
+					<div class="masthead">
+						<div class="container-head">
+							<h1><?php echo $nome_cliente ?><a class="btn btn-default btn-sm pull-right" href="<?php echo base_url('Conta/desconectar')?>" role="button">Sair  <span class="fa fa-sign-out" aria-hidden="true"></span></a></h1>
+						</div>
+					</div>
+				</div><!-- /col-md-12 -->
+			</div><!-- /row-fluid -->
+		<!-- /header-->
 		
-		<!-- CONTENT -->
-			
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h4><?php  echo 'Esta&ccedil;&atilde;o ' . $nome_estacao ?></h4>
-				</div>
-				<div class="panel-body">
-					<p><?php  echo 'Per&iacute;odo: ' . $data_inicial . ' - ' . $data_final ?></p>
-					<!-- table -->
-					<div class="table-responsive">
-						
-						<?php echo $tabela  ?>
-						
-					</div>						
-					<!-- table -->
-			
-				</div>
-			</div>	
-			
-		<!-- CONTENT -->
-		
-		<!-- FOOTER -->
-		<div class="row">
-			<div class="col-md-12">
-				<a class="btn btn-default pull-left" href='<?php echo base_url("Estacao/pesquisar")?>' role="button"><span class="fa fa-chevron-left" aria-hidden="true"></span></a>
-				<a class="btn btn-default pull-right" href='<?php echo base_url("Pesquisa/exportar_csv?nome_estacao={$nome_estacao}&data_inicial={$data_inicial}&data_final={$data_final}")?>' role="button"><span class="fa fa-file-excel-o" aria-hidden="true"></span></a>
-			</div>
-		</div>
-		<!-- FOOTER -->
-		
+		<!-- main content -->
+			<div class="row-fluid">
+				<div class="col-md-12">
+					
+					<div class="panel panel-info">
+						<div class="panel-heading">
+							<div class="row">
+								<div class="col-md-4"><a class="btn btn-sm pull-left" href='<?php echo base_url("Estacao/pesquisar")?>' role="button"><span class="fa fa-chevron-left" aria-hidden="true"> voltar</span></a></div>
+								<div class="col-md-4"><h4 class="text-center"><strong><?php  echo 'Esta&ccedil;&atilde;o ' . $nome_estacao ?></strong></h4></div>
+								<div class="col-md-4"><a class="btn btn-sm pull-right" href='<?php echo base_url("Pesquisa/exportar_csv?nome_estacao={$nome_estacao}&data_inicial={$data_inicial}&data_final={$data_final}")?>' role="button">exportar <span class="fa fa-file-excel-o" aria-hidden="true"></span></a></div>
+							</div>
+						</div>
+  						<div class="panel-body">
+  							<div class="panel panel-default">
+								<div class="panel-heading">
+									<p><?php  echo 'Per&iacute;odo: ' . $data_inicial . ' - ' . $data_final ?></p>
+								</div>
+  								<div class="panel-body">
+  									<div class="table-responsive">
+										<?php echo $tabela  ?>
+									</div>
+  								</div>
+  							</div>
+  						</div>
+  					</div>
+  				</div>
+			</div><!-- /row-fluid -->
 	</div>
-	<!-- CONTAINER -->
 </body>
+
 </html>
